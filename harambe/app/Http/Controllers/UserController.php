3173d5819a758713e8133b49e0d5a971ef194b38
Joakim <Joakim.Lien@stud.hials.no>
2016-11-19 00:11:03 +0100

    UserController functions added

--- harambe/app/Http/Controllers/UserController.php
+++ harambe/app/Http/Controllers/UserController.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function getLogin(){
+
+    }
+
+    public function postLogin(){
+
+    }
+
+    public function getRegister(){
+
+    }
+
+    public function postRegister(){
+
+    }
+
+    public function getProfile(){
+
+    }
+
+    public function getEdit(){
+
+    }
+
+    public function postEdit(){
+    	
+    }
 }

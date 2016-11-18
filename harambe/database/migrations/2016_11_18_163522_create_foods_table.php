ad8582269e0a9f1c79c3d7764e1feb5ab360acb0
Joakim <Joakim.Lien@stud.hials.no>
2016-11-18 17:38:06 +0100

    Added Food model with migration

---
+++ harambe/database/migrations/2016_11_18_163522_create_foods_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->string('food_id');
+            $table->string('name');
+            $table->string('imagePath');
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->primary('food_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}

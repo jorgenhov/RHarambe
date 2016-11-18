3f77e6a637bf94d1ecc44849709458165060d691
Joakim <Joakim.Lien@stud.hials.no>
2016-11-19 00:40:44 +0100

    OrderController functions added

--- harambe/app/Http/Controllers/OrderController.php
+++ harambe/app/Http/Controllers/OrderController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function getTakeaway(){
+
+    }
+
+    public function getTableReservation(){
+
+    }
+
+    public fucntion postTableReservation(){
+
+    }
+
+    public function getFoodReservation(){
+
+    }
+
+    public function postFoodReservation(){
+    	
+    }
 }

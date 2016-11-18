ec32edb421261fbcdc4ef24781ce3b087713c4ce
Joakim <Joakim.Lien@stud.hials.no>
2016-11-18 17:30:05 +0100

    Added Table model with migration

---
+++ harambe/database/migrations/2016_11_18_162814_create_tables_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->string('table_id');
+            $table->string('name');
+            $table->integer('capacity');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->primary('table_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tables');
+    }
+}

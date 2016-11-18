1833b67995326d96e1d5dba45692dbc7f2e10f27
Joakim <Joakim.Lien@stud.hials.no>
2016-11-18 17:42:12 +0100

    Added PersonOrder model with migration

---
+++ harambe/database/migrations/2016_11_18_164029_create_person_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('person_orders', function (Blueprint $table) {
+            $table->string('person_order_id');
+            $table->string('order_id');
+            $table->string('food_id');
+            $table->string('drink_id');
+            $table->timestamps();
+
+            $table->primary('person_order_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('person_orders');
+    }
+}

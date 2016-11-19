76f5fe0d29738091af660f294f9711247a0080f1
Joakim <Joakim.Lien@stud.hials.no>
2016-11-19 23:40:13 +0100

    Takeaway controller updated

--- harambe/app/Http/Controllers/OrderController.php
+++ harambe/app/Http/Controllers/OrderController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Food;
 
 class OrderController extends Controller
 {
     public function getTakeaway(){
-
+        $food = Food::all();
+        $foodCollect = collect($food);
+        $firstRow = $foodCollect->only(0,1,2,3);
+        $secondRow = $foodCollect->only(4,5,6,7);
+        return view('order.takeaway',[
+            'firstRow' => $firstRow,
+            'secondRow' => $secondRow
+        ]);
     }
 
     public function getTableReservation(){

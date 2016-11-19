e235d17cf4cd578872b63b04f6634d3be7942be1
Joakim <Joakim.Lien@stud.hials.no>
2016-11-19 01:07:41 +0100

    Added folders to route file

--- harambe/routes/web.php
+++ harambe/routes/web.php
@@ -14,3 +14,88 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+Route::group(['prefix' => 'admin'], function(){
+	Route::get('',[
+		'uses' => '',
+		'as' => ''
+	]);
+});
+
+
+
+
+Route::group(['prefix' => 'order'], function(){
+	Route::get('foodreservation',[
+		'uses' => 'OrderController@getFoodReservation',
+		'as' => 'order.foodreservation'
+	]);
+	Route::get('tablereservation',[
+		'uses' => 'OrderController@getTableReservation',
+		'as' => 'order.tablereservation'
+	]);
+	Route::get('takeaway',[
+		'uses' => 'OrderController@getTakeaway',
+		'as' => 'order.takeaway'
+	]);
+});
+
+
+
+
+Route::group(['prefix' => 'pages'], function(){
+	Route::get('index',[
+		'uses' => 'PageController@getIndex',
+		'as' => 'pages.index'
+	]);
+	Route::get('takeaway',[
+		'uses' => 'PageController@getContact',
+		'as' => 'pages.contact'
+	]);
+});
+
+
+
+
+Route::group(['prefix' => 'shop'], function(){
+	Route::get('cart',[
+		'uses' => 'ShopController@getUserCart',
+		'as' => 'shop.cart'
+	]);
+	Route::get('addToCart',[
+		'uses' => 'ShopController@addToCart',
+		'as' => 'pages.contact'
+	]);
+});
+
+
+
+Route::group(['prefix' => 'users'], function(){
+	Route::get('login',[
+		'uses' => 'UserController@getLogin',
+		'as' => 'user.login'
+	]);
+	Route::post('login',[
+		'uses' => 'UserController@postLogin',
+		'as' => 'user.login'
+	]);
+	Route::get('register',[
+		'uses' => 'UserController@getRegister',
+		'as' => 'user.register'
+	]);
+	Route::post('register',[
+		'uses' => 'UserController@postRegister',
+		'as' => 'user.register'
+	]);
+	Route::get('edit',[
+		'uses' => 'UserController@getEdit',
+		'as' => 'user.edit'
+	]);
+	Route::post('edit',[
+		'uses' => 'UserController@postEdit',
+		'as' => 'user.edit'
+	]);
+});

d7ff7038e5eaebb29ea606f3c7c14d4481cb8709
Joakim <Joakim.Lien@stud.hials.no>
2016-11-19 05:22:34 +0100

    Fixed person model

--- harambe/app/Person.php
+++ harambe/app/Person.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
+	protected $fillable = ['person_id','firstname','lastname','email','phonenumber','adress'];
+
     public function orders(){
     	return $this->hasMany('App\Order');
     }

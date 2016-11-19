8621abab1b7d9abeae6bcc65a9b7e524f3e930c0
Joakim <Joakim.Lien@stud.hials.no>
2016-11-19 21:53:14 +0100

    Fillablle added to Food model

--- harambe/app/Food.php
+++ harambe/app/Food.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Food extends Model
 {
+	protected $fillable = ['food_id','name','imagePath','price','description'];
+	
     public function personOrders(){
     	return $this->hasMany('App\PersonOrder');
     }

7cc9a4877f6475f77bac41923a541308f2556898
Joakim <Joakim.Lien@stud.hials.no>
2016-11-19 23:33:18 +0100

    Table seeder fix

--- harambe/database/seeds/TableTablesSeeder.php
+++ harambe/database/seeds/TableTablesSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Table;
 
 class TableTablesSeeder extends Seeder
 {
@@ -15,42 +16,42 @@ class TableTablesSeeder extends Seeder
             'table_id' => '1OBBCcTcMg2koe0d',
             'name' => 1,
             'capacity' => 1,
-            'description' => 'Sitt for deg selv og nyt en god middag med god havutsikt.',
+            'description' => utf8_encode('Sitt for deg selv og nyt en god middag med god havutsikt.'),
         ]);
         $table->save();
         $table = new Table([
             'table_id' => 'Ie4QPP6kk3kXYWJp',
             'name' => 2,
             'capacity' => 2,
-            'description' => 'Ta med deg noen, og sitt ved et av våre to personers bord.',
+            'description' => utf8_encode('Ta med deg noen, og sitt ved et av våre to personers bord.'),
         ]);
         $table->save();
         $table = new Table([
             'table_id' => '3oFg2ruwtGeRYlHF',
             'name' => 3,
             'capacity' => 4,
-            'description' => 'Vårt fire personers bord ligger ved vinduet, slik at dere kan nyte utsikten.',
+            'description' => utf8_encode('Vårt fire personers bord ligger ved vinduet, slik at dere kan nyte utsikten.'),
         ]);
         $table->save();
         $table = new Table([
             'table_id' => 'qV5eXz7fsfEYFTVh',
             'name' => 4,
             'capacity' => 3,
-            'description' => 'Et av våre flotteste bord helt ved vinduet.',
+            'description' => utf8_encode('Et av våre flotteste bord helt ved vinduet.'),
         ]);
         $table->save();
         $table = new Table([
             'table_id' => 'CVkqSEGbY3yvfuie',
             'name' => 5,
             'capacity' => 6,
-            'description' => 'Det største bordet vi kan tilby som er avskildt fra andre bord.',
+            'description' => utf8_encode('Det største bordet vi kan tilby som er avskildt fra andre bord.'),
         ]);
         $table->save();
         $table = new Table([
             'table_id' => 'DnAqWvTRF83AVbPD',
             'name' => 6,
             'capacity' => 3,
-            'description' => 'Vårt V.I.P bord ligger ved vinduet og er helt ved inngangen',
+            'description' => utf8_encode('Vårt V.I.P bord ligger ved vinduet og er helt ved inngangen'),
         ]);
         $table->save();
     }
